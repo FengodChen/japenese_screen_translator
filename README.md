b3aba83fa7f071b9f4018ae3b0e73cc322d6b91e
Liao Chen <a1176553293@outlook.com>
2024-01-12 19:14:23 +0800

    update

---
+++ README.md
@@ -0,0 +1,15 @@
+# 介绍
+本应用基于Python，用于自动截取电脑屏幕上的图片，用OCR技术识别图片中的日语，并通过神经网络将日语翻译成中文。日语和中文会显示在应用的GUI中。
+
+# 安装
+0. 建议使用conda或venv的虚拟环境安装python包
+1. 安装Python（此部分可参考其他网站安装）
+2. 安装GPU版本的Pytorch：`python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118`
+3. 安装其他依赖：`python -m pip install -r requirements.txt`
+
+# 运行
+0. 第一次运行时会自动下载神经网络算法权重
+1. 点击“选择翻译区域”，用鼠标选择翻译区域后，按键盘上的ESC键确定
+2. 选择“运行”或“自动运行”，即可运行
+
+![](./asset/example.png)
--- utils/processes.py
+++ utils/processes.py
@@ -4,6 +4,21 @@ from .screen_cropper import Screen_Cropper
 from .ocr import Japenese_OCR
 from .translator import Translator
 
+import torch
+from torchvision.transforms import ToTensor
+
+__to_tensor = ToTensor()
+
+def __judge_diff(img1, img2):
+    img1 = __to_tensor(img1) if img1 != None else torch.zeros(1)
+    img2 = __to_tensor(img2)
+
+    if img1.size() != img2.size():
+        mse = 1e12
+    else:
+        mse = (img1 - img2).pow(2).mean().item()
+    return mse
+
 def gui_main_process(p1, p2):
     g = GUI_Main(p1, p2)
     g.run()
@@ -32,13 +47,25 @@ def orc_translate_subprocess(dev, pipe_mainGUI):
     japenese_ocr = Japenese_OCR(dev=dev)
     translator = Translator(src_lang="ja", target_lang="zh", dev=dev)
 
+    pre_img = None
+    pre_ja_text = ""
+    pre_zh_text = ""
+    MIN_MSE = 0.0001
+
     while True:
         try:
             msg = pipe_mainGUI.recv()
             if msg["comm"] == "start ocr and translate":
                 img = screen_cropper.crop()
-                ja_text = japenese_ocr.get_text(img)
-                zh_text = translator.translate(ja_text)
+                if __judge_diff(pre_img, img) > MIN_MSE:
+                    ja_text = japenese_ocr.get_text(img)
+                    zh_text = translator.translate(ja_text)
+                    pre_ja_text = ja_text
+                    pre_zh_text = zh_text
+                else:
+                    ja_text = pre_ja_text
+                    zh_text = pre_zh_text
+                pre_img = img
                 msg = dict(
                     comm = "push crop img",
                     data = (ja_text, zh_text)
